9a6028d789d92e4c7191b38269f579c34948ffa4
Taisa Costa <taisadaiana@yahoo.com.br>
2021-10-05 22:45:20 -0300

    transforma movie widget em statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class AnthorflixApp extends StatefulWidget {
 }
 
 class _MainWindow extends State<AnthorflixApp> {
-  Movie _movie = Movie(imdbId: "id", title: "title", poster: "poster", year: "year",
+  Movie _movie = Movie(imdbId: "id", title: "title", poster: "https://www.uninassau.edu.br/sites/mauriciodenassau.edu.br/files/fields/imagemLateral/noticias/2018/04/700_fo38843930_9e45ae83c58d8b25cef06bb9ea6a7a05.jpg", year: "year",
       director: "director", actors: "actors", genre: "genre", released: "released",
       plot: "plot");
   String _movie_name = " ";
--- lib/movies_widget.dart
+++ lib/movies_widget.dart
@@ -4,21 +4,31 @@ import 'package:anthorflix/movie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class MoviesWidget extends StatelessWidget{
-  final Movie movie;
+class MoviesWidget extends StatefulWidget {
+  Movie movie;
   MoviesWidget({required this.movie});
+  @override
+  _MoviesWidget createState() => _MoviesWidget();
+}
+
+class _MoviesWidget extends State<MoviesWidget>{
+
+  @override
+  void initState(){
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context){
     return ListView(
         children: [
           Text(
-            movie.title,
+            widget.movie.title,
             textAlign: TextAlign.center,
             style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30)
           ),
           Text(
-            movie.year,
+            widget.movie.year,
             textAlign: TextAlign.center,
             style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.green )
           ),
@@ -27,36 +37,38 @@ class MoviesWidget extends StatelessWidget{
               minRating: 0,
               itemBuilder: (context, _) => Icon(Icons.star, color: Colors.amber),
               onRatingUpdate: (rating) {
-                movie.rating = rating;
+                setState(() {
+                  widget.movie.rating = rating;
+                });
               },
             ),
           ),
-          Image.network(movie.poster),
+          Image.network(widget.movie.poster),
           const Text(
               "Elenco",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
           ),
-          Text(movie.actors),
+          Text(widget.movie.actors),
           const Text(
               "Diretor(a):",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
           ),
-          Text(movie.director),
+          Text(widget.movie.director),
           const Text(
               "Data do lançamento:",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
           ),
-          Text(movie.released),
+          Text(widget.movie.released),
           const Text(
               "Gênero:",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
           ),
-          Text(movie.genre),
+          Text(widget.movie.genre),
           const Text(
               "Descrição:",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
           ),
-          Text(movie.plot),
+          Text(widget.movie.plot),
           const Text(
               "Comentários:",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
@@ -64,4 +76,5 @@ class MoviesWidget extends StatelessWidget{
       ],
     );
   }
-}
+
+ }
